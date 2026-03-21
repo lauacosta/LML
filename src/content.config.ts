1c15c8e63493ddc315668f780846e95d7f173e96
Lautaro Acosta Quintana <acostaquintanalautaro@gmail.com>
2026-03-21 16:40:39 -0300

    Reemplazo /blog por /desafios

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,17 @@
-import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
-		}),
+const desafios = defineCollection({
+    loader: glob({ base: "./src/content/desafios", pattern: "**/*.{md,mdx}" }),
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            description: z.string(),
+            pubDate: z.coerce.date(),
+            updatedDate: z.coerce.date().optional(),
+            heroImage: z.optional(image()),
+        }),
 });
 
-export const collections = { blog };
+export const collections = { desafios };
