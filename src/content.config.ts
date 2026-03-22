be8b06a3bf3b816750f3ecf2740b1004389935e0
StegmayerTobias <tobiassebastianstegmayer@gmail.com>
2026-03-22 13:01:24 -0300

    Add Lenis for smooth scrolling and refactor layout structure

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,15 +3,15 @@ import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
 const desafios = defineCollection({
-    loader: glob({ base: "./src/content/desafios", pattern: "**/*.{md,mdx}" }),
-    schema: ({ image }) =>
-        z.object({
-            title: z.string(),
-            description: z.string(),
-            pubDate: z.coerce.date(),
-            updatedDate: z.coerce.date().optional(),
-            heroImage: z.optional(image()),
-        }),
+  loader: glob({ base: "./src/content/desafios", pattern: "**/*.{md,mdx}" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: z.optional(image()),
+    }),
 });
 
 export const collections = { desafios };
