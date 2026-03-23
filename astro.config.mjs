a77700f639af129e07004500bea3287072d8ec6b
Lautaro Acosta Quintana <acostaquintanalautaro@gmail.com>
2026-03-23 09:18:44 -0300

    Algunas optimizaciones para respetar la configuracion de los usuarios. Declaro el uso de sharp en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import sharp from 'sharp';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	image: {
+		service: {
+			entrypoint: 'astro/assets/services/sharp',
+		},
+	},
 });
