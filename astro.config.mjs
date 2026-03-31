8735ef6e2b13139da86292db868eff52b70111a4
StegmayerTobias <tobiassebastianstegmayer@gmail.com>
2026-03-31 14:35:55 -0300

    sitio oficial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-import sharp from 'sharp';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import sharp from "sharp";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	image: {
-		service: {
-			entrypoint: 'astro/assets/services/sharp',
-		},
-	},
+  site: "https://lmlgestion.com.ar",
+  integrations: [mdx(), sitemap()],
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+    },
+  },
 });
